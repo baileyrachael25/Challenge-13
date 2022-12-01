fe58c8ba1945074c9494af41ec2ad81454e4cfda
baileyrachael25 <baileyrachael25@gmail.com>
2022-11-30 21:43:59 -0800

    intial commit

---
+++ Develop/models/index.js
@@ -0,0 +1,41 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+const sequelize = require('../config/connection');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Categories have many Products
+Category.hasMany(Product {
+  foreginKey: 'category_id',
+  onDelete: CASCADe
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreginKey: 'product_id'
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany (Product {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
