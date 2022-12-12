8813d99cb646bcff21f8774149260ec358855732
baileyrachael25 <baileyrachael25@gmail.com>
2022-12-12 01:46:00 -0800

    fixed product-routes and edited readme

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-## Title
+## ORM E-commerce Back End
 
 ## Description
+This application is a fully functioning back-end api for an e-commerce website. It allows the user to view products, tags, and categories using the GET, POST, PUT, and DELETE route methods.
 
 ## Table of Contents
 - [Installation](#install)
 - [Video Demo](#demo)
-- [Usage](#usage)
 - [Questions](#questions)
 
 
 ## Installation
+After cloning the repo, make sure you have node installed on your machine.
+Then you can run the 'npm i' command to install the packages for the needed dependencies.
+You will also need to 'cd' into the 'db' file so that you can create the database tables using the 'source schema.sql' command.
+Exit mysql, and then run 'npm start' to start the server so that you can start creating requests to the http server at localhost:3001 in Insomnia.
 
 ## Video Demo
 
-## Usage
 
 ## Questions
 Any questions about this code? Feel free to contact me on GitHub at https://github.com/baileyrachael25 or contact me via email at baileyrachael25@gmail.com
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,13 +41,32 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
-  try {
-    const productData = await Product.create(req.body)
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+router.post('/', (req, res) => {
+  Product.create(req.body)
+
+    .then((product) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      
+      if (req.body.tagIds.length) {
+        const productTagIds = req.body.tagIds.map((tag_id) => {
+          return {
+            product_id: product.id,
+            tag_id,
+          };
+        });
+        return ProductTag.bulkCreate(productTagIds);
+      }
+      // if no product tags, just respond
+      res.status(200).json(product);
+    })
+    
+    .then((productTagIds) => res.status(200).json(productTagIds))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -56,27 +75,43 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-});
 
 // update product
-router.put('/:id', async (req, res) => {
+router.put('/:id', (req, res) => {
   // update product data
-  try {
-    const productData = await Product.update(req.body, {
-      where: {
-        id: req.params.id,
-      }
-    });
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
 
-    if (!productData) {
-      res.status(404).json({ message: 'No Product found with this id' });
-      return;
-    }
+  .then((productData) => {
+    return ProductTag.findAll({ where: { product_id: req.params.id } });
+  })
 
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  .then((productTags) => {
+    const productTagIds = productTags.map(({ tag_id }) => tag_id);
+    const newProductTags = req.body.tagIds
+      .filter((tag_id) => !productTagIds.includes(tag_id))
+      .map((tag_id) => {
+        return {
+          product_id: req.params.id,
+          tag_id,
+        };
+      });
+    const productTagsToRemove = productTags
+      .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+      .map(({ id }) => id);
+    return Promise.all([
+      ProductTag.destroy({ where: { id: productTagsToRemove } }),
+      ProductTag.bulkCreate(newProductTags),
+    ]);
+  })
+
+  .then((updatedProductTags) => res.json(updatedProductTags))
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 router.delete('/:id', async (req, res) => {
